51db3daee21837b1295c02dc1797fb76b75966a3
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-12-14 14:21:55 +0100

    final

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import cv2
 import mediapipe as mp
+import RPi.GPIO as GPIO
+import time
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mp_drawing = mp.solutions.drawing_utils
+LED_PIN1 = 14
+LED_PIN2 = 17
+LED_PIN3 = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_PIN1, GPIO.OUT)
+GPIO.setup(LED_PIN2, GPIO.OUT)
+GPIO.setup(LED_PIN3, GPIO.OUT)
 
 
 def detect_thumbs_up(landmarks):
@@ -51,6 +61,25 @@ def detect_open_hand(landmarks):
     return False
 
 
+def light_up_led():
+    GPIO.output(LED_PIN1, GPIO.LOW)
+    GPIO.output(LED_PIN2, GPIO.LOW)
+
+
+def light_down_led():
+    GPIO.output(LED_PIN1, GPIO.HIGH)
+    GPIO.output(LED_PIN2, GPIO.HIGH)
+
+
+def blink(duration=0.5):
+    start = time.time()
+    while time.time() - start < duration:
+        GPIO.output(LED_PIN3, GPIO.HIGH)
+        time.sleep(1.0)
+        GPIO.output(LED_PIN3, GPIO.LOW)
+        time.sleep(1.0)
+
+
 last_gesture = None
 
 cap = cv2.VideoCapture(0)
@@ -69,10 +98,13 @@ while cap.isOpened():
         for hand_landmarks in result.multi_hand_landmarks:
             if detect_thumbs_up(hand_landmarks.landmark):
                 current_gesture = "Kciuk w górę!"
+                light_up_led()
             elif detect_thumbs_down(hand_landmarks.landmark):
                 current_gesture = "Kciuk w dół!"
+                light_down_led()
             elif detect_open_hand(hand_landmarks.landmark):
                 current_gesture = "Otwarta dłoń!"
+                blink()
             else:
                 current_gesture = "Inny gest"
 
@@ -87,6 +119,8 @@ while cap.isOpened():
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
+
+GPIO.cleanup()
 
 cap.release()
 cv2.destroyAllWindows()
